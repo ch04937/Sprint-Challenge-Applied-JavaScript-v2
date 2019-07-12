6c73e5d8e9b3a77ce5e1336de085cb8e01039c2f
unknown <ch04937>
2019-07-12 12:07:37 -0700

    'completed cards'

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,6 +1,23 @@
 // STEP 3: Create Article cards.
 // -----------------------
 // Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(data => {
+    const articles = data.data.articles; 
+    const articleTopic = Object.keys(articles);
+
+    articleTopic.map(topics => {
+      articles[`${topics}`].map(article => {
+        cardsContainer.append(createCard(article));
+      });
+    });
+  })
+.catch (err => {
+    console.log('there is an error', err)
+})
+
+
 // Stduy the response data you get back, closely.
 // You will be creating a component for each 'article' in the list.
 // This won't be as easy as just iterating over an array though.
@@ -17,3 +34,35 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+const cardsContainer = document.querySelector('.cards-container')
+
+function createCard (object) {
+    const card = document.createElement('div')
+    const headline = document.createElement('div')
+    const authorContainer = document.createElement('div')
+    const imgContainer = document.createElement('div')
+    const img = document.createElement('img')
+    const author = document.createElement('span')
+
+    card.classList.add('card')
+    headline.classList.add('headline')
+    authorContainer.classList.add('author')
+    imgContainer.classList.add('img-container')
+
+    card.appendChild(headline)
+    card.appendChild(authorContainer)
+    authorContainer.appendChild(imgContainer)
+    authorContainer.appendChild(author)
+    imgContainer.appendChild(img)
+
+    img.src = object.authorPhoto
+    headline.textContent = object.headline
+    author.textContent = object.authorName
+
+
+    return card;
+
+
+
+
+}
